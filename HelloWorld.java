edeac03ee4c1c8dc5ae3eb686fc2615d442464c0
lamothe1998 <lamothe1998@gmail.com>
2021-09-08 16:52:16 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,3 @@
-git clone https://github.com/lamothe1998/FirstProject/new/main
-cd into_new_repository
-cp java_file_path HelloWorld.java
-git add . 
-git commit -m "Hello World program"
-git push 
 
 public class HelloWorld{
     public void main(String[] args){
