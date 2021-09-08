999cb1e2935367290ab0564c1daa33ac56e1ccbe
lamothe1998 <lamothe1998@gmail.com>
2021-09-08 16:50:05 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+git clone https://github.com/lamothe1998/FirstProject/new/main
+cd into_new_repository
+cp java_file_path HelloWorld.java
+git add . 
+git commit -m "Hello World program"
+git push 
+
+public class HelloWorld{
+    public void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
